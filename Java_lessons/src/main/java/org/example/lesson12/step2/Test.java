c54bf7019a2a0f14d113fbf0b431962e1bb8a4a8
gera <gluk19931123@gmail.com>
2026-03-15 13:42:37 +0300

    Урок 12 и 13

---
+++ Java_lessons/src/main/java/org/example/lesson12/step2/Test.java
@@ -0,0 +1,13 @@
+package org.example.lesson12.step2;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Getter
+public class Test {
+    private int id;
+    private String name;
+}
